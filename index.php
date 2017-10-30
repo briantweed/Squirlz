16b505753a5e7d3737b59cef8de8f2bcfc127667
Brian Tweed <brtweed@outlook.com>
2017-10-30 14:46:00 +0000

    no message

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Squirlz</title>
 
@@ -47,23 +47,25 @@
 
         <link rel="author" href="humans.txt">
 
-        <link rel="prefetch" href="css/styles.css?v=<?php echo VERSION ?>">
+        <link rel="prefetch" href="css/styles.css?v=<?php echo VERSION ?>" rel="stylesheet">
         <link rel="prefetch" href="images/background.jpg">
         <link rel="prefetch" href="images/square.png">
         <link rel="prefetch" href="images/empty.png">
 
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
-        <link rel="stylesheet" href="css/styles.css?v=<?php echo VERSION ?>">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" rel="stylesheet">
+        <link href="css/styles.css?v=<?php echo VERSION ?>" rel="stylesheet">
 
     </head>
 
     <body>
 
 
+
         <nav class="navbar navbar-dark bg-dark">
             <a class="navbar-brand" href="#">Squirlz</a>
         </nav>
+
 
 
         <div id="wrapper" class="container-fluid">
@@ -89,7 +91,7 @@
                     <div v-for="(squirl, index) in squirlz" :class="squirl.class">
                         <div class="card" :data-num="index">
                             <a href="javascript:void(0)" v-on:click="selectSquirl(squirl.name, index)">
-                                <img class="rounded img-fluid pb-2" :src="squirl.image" alt="">
+                                <img class="rounded img-fluid pb-2" :src="squirl.image" :alt="squirl.name">
                                 <h4 class="text-primary text-center">{{squirl.name}}</h4>
                             </a>
                         </div>
@@ -106,8 +108,8 @@
                 <div class="row">
                     <div class="grid offset-1 col-5">
                         <div v-for="acorn in acorns" class="acorn">
-                            <span v-if="acorn.active"><img :num="acorn.number" src="images/square.png" class="img-fluid" v-on:click="toggle(acorn)"></span>
-                            <span v-else><img :num="acorn.number" src="images/empty.png" class="img-fluid" v-on:click="toggle(acorn)"></span>
+                            <span v-if="acorn.active"><img :num="acorn.number" src="images/square.png" alt="acorn" class="img-fluid" v-on:click="toggle(acorn)"></span>
+                            <span v-else><img :num="acorn.number" src="images/empty.png" alt="empty" class="img-fluid" v-on:click="toggle(acorn)"></span>
                         </div>
                     </div>
                     <div class="col-5">
@@ -171,18 +173,21 @@
         <!-- /.container-fluid -->
 
 
+
         <footer class="text-center">
             &copy; brtweed.co.uk
         </footer>
 
 
-        <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/jquery-validation@1.17.0/dist/jquery.validate.min.js" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
-        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/vue"></script>
-        <script type="text/javascript" src="https://js.pusher.com/4.1/pusher.min.js"></script>
-        <script type="text/javascript" src="js/script.js?v=<?php echo VERSION ?>"></script>
+
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" type="text/javascript" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.17.0/dist/jquery.validate.min.js" type="text/javascript" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" type="text/javascript" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" type="text/javascript" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/vue" type="text/javascript"></script>
+        <script src="https://js.pusher.com/4.1/pusher.min.js" type="text/javascript"></script>
+        <script src="js/script.js?v=<?php echo VERSION ?>" type="text/javascript"></script>
+
 
     </body>
 
